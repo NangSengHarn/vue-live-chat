f71d5fb8a9e8869b07db40aba734b848ae6fb40d
Nang Seng Harn <nangsengharn@gmail.com>
2021-08-30 13:03:00 +0630

    firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+const firebaseConfig = {
+    apiKey: "AIzaSyDqqF9LTGRJekZi99L7VHbqDo7eMUekrZM",
+    authDomain: "vue-content-post.firebaseapp.com",
+    projectId: "vue-content-post",
+    storageBucket: "vue-content-post.appspot.com",
+    messagingSenderId: "145454486093",
+    appId: "1:145454486093:web:5ffe02e40b0c2284194204"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+let db =firebase.firestore();
+let timestamp= firebase.firestore.FieldValue.serverTimestamp;
+
+export {db,timestamp};
