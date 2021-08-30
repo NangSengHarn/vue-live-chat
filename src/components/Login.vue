1829733c91bcdd6dd65d02bb8d024d806c2fae4f
Nang Seng Harn <nangsengharn@gmail.com>
2021-08-30 13:49:48 +0630

    Add login and signup form design

---
+++ src/components/Login.vue
@@ -0,0 +1,27 @@
+<template>
+  <h2>LogIn</h2>
+  <form @submit.prevent="logIn">
+      <input type="email" placeholder="email" v-model="email">
+      <input type="password" placeholder="password" v-model="password">
+      <button>LogIn</button>
+  </form>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity'
+export default {
+    setup(){
+        let email=ref("");
+        let password=ref("");
+        let logIn=()=>{
+            console.log(email.value,password.value);
+        }
+
+        return {email,password,logIn};
+    }
+}
+</script>
+
+<style>
+
+</style>
