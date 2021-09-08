f9c73d34f6d885df7bd505de533b07bc136849de
Nang Seng Harn <nangsengharn@gmail.com>
2021-09-08 16:07:51 +0630

    Create Chatwindow and fetch message onSnapshot

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="container">
     <Navbar></Navbar>
+    <Chatwindow></Chatwindow>
     <NewChatForm></NewChatForm>
   </div>
 </template>
 
 <script>
+import Chatwindow from '../components/Chatwindow'
 import NewChatForm from '../components/NewChatForm'
 import { watch } from '@vue/runtime-core'
 import Navbar from '../components/Navbar'
@@ -13,6 +15,7 @@ import getUser from '../composables/getUser'
 import {useRouter} from 'vue-router'
 export default {
   components: {
+    Chatwindow,
     NewChatForm, Navbar },
   setup(){
     let {user}=getUser();
